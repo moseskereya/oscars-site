be7c20efda4e36dcd6baa8833cdcdb88a3d0eff4
MOSES <MOSESLAZARO2019@outlook.com>
2019-09-21 10:39:25 +0300

    Updates

---
+++ precache-manifest.8f7cdff1b4ea6e8ff4eda45c356496c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ac91325f792506cc0b21eba5dd5640",
+    "url": "/oscars-site/index.html"
+  },
+  {
+    "revision": "fc43650cfdc1dee02348",
+    "url": "/oscars-site/static/css/main.e4306daf.chunk.css"
+  },
+  {
+    "revision": "18a5ef48cfbd84e386fe",
+    "url": "/oscars-site/static/js/2.e4385987.chunk.js"
+  },
+  {
+    "revision": "fc43650cfdc1dee02348",
+    "url": "/oscars-site/static/js/main.b1fb9ef5.chunk.js"
+  },
+  {
+    "revision": "948455a39a9f42612375",
+    "url": "/oscars-site/static/js/runtime-main.3a67fba5.js"
+  }
+]);
